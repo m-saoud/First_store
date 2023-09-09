0cf96063d73414b8815dd2fc9e4a9519f8b353b1
m-saoud <131665242+m-saoud@users.noreply.github.com>
2023-09-09 15:04:24 +0900

    making the footer & chang scc

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,25 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import button from '@/components/buttom/button'
+import Hero from '/src/images/hero.svg'
 
 export default function Home() {
   return (
-  <h1>HOME</h1>
+    <div className={styles.container}>
+      <div className={styles.col}>
+        <h1 className={styles.title}>Your One Stop online pets store Destination</h1>
+        <p className={styles.description}>Discover the world of enless showed by home</p>
+        <button className={styles.button}>Shop Now</button>
+      </div>
+      <div className={styles.col}>
+        <Image  className={styles.image}   src={Hero} alt=''></Image>
+      
+      </div>
+
+
+
+
+    </div>
+  
   )
 }
--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import styles from './footer.module.css';
+import Image from 'next/image';
+
 
 const footer = () => {
   return (
-    <div>
-      <h1>footer</h1>
+    <div className={styles.container}>
+      <div>© 2023 Cockapo. Al right reserved</div>
+      <div className={styles.social}>
+      <link rel="icon" type="image/png"></link>
+        <Image
+          src='/src/images/s.m.icons/facebook_4494475.png'
+          width={10}
+          height={10}
+          className={styles.icon}
+          alt='Cockapo facebook link'
+        ></Image>
+        <Image
+          src='/src/images/s.m.icons/instagram.png'
+          width={10}
+          height={10}
+          className={styles.icon}
+          alt='Cockapo instagram link'
+        ></Image>
+        <Image
+          src='/src/images/s.m.icons/youtube.png'
+          width={10}
+          height={10}
+          className={styles.icon}
+          alt='Cockapo youtube link'
+        ></Image>
+        
+      </div>
     </div>
   )
 }
