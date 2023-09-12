37f8d623afc6afa64bc33be812d168beb95b6f0f
m-saoud <131665242+m-saoud@users.noreply.github.com>
2023-09-13 00:19:29 +0900

    SEO Pages

--- src/app/(dynamic)/blog/[id]/page.jsx
+++ src/app/(dynamic)/blog/[id]/page.jsx
@@ -2,22 +2,44 @@ import React from 'react'
 import styles from './page.module.css'
 import Image from 'next/image'
 
-const post = () => {
+
+
+
+async function getData(id) {
+  const res = await fetch(`https://dummyjson.com/products/${id}`)
+  
+ 
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return res.json()
+}
+ 
+
+
+   async function post ({ params }) {
+  const product = await getData(params.id)
+
   return (
     <div className={styles.container}>
       <header className={styles.header}>
         <div className={styles.info}>
-          <h1 className={styles.title}>xx</h1>
-          <p className={styles.desc}>xxx</p>
+          <h1 className={styles.title}>{product.title}</h1>
+          <p className={styles.desc}>{product.description}</p>
         </div>
         <div className={styles.imageContainer}>
-          <Image alt='image id' fill={true} src="https://images.pexels.com/photos/3687770/pexels-photo-3687770.jpeg" className={styles.image}></Image>
+          <Image alt='image id' fill={true} src={product.thumbnail} className={styles.image}></Image>
         </div>
-        <span className={styles.auther}>Saoud.S</span>
+        <span className={styles.auther}>{`${product.price} $`}</span>
       </header>
       <div className={styles.containt}>
-        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione animi exercitationem debitis incidunt eveniet dicta quos molestiae iste quidem, recusandae, eos numquam.
-          Quisquam ipsam deleniti facere, quam ullam, iusto sit delectus excepturi repellat corporis optio necessitatibus rem cumque quo dignissimos blanditiis quas ut nobis qui? Cupiditate dolores et corrupti similique.</p>
+      </div>
+      <div className={styles.category}>
+        {product.images.map((image) =>
+          <Image key={image.id} alt='pro_Image' width={200} height={240} className={styles.prouactImages} src={image}></Image>
+        )}
+        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim est quo nam a deleniti omnis excepturi. Voluptatibus numquam totam, accusamus quam ab eum quaerat. Doloremque alias, voluptates quam, maiores exercitationem accusamus velit sit dolorem esse reiciendis voluptatum eius mollitia distinctio?</p>
       </div>
     </div>
   )
